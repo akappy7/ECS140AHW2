dee4000716305ed7fca0e0a11e22327370287fbf
Arman Shakirt Kapbasov <akap@pc5.cs.ucdavis.edu>
2014-10-18 18:37:33 -0700

    Initial given

---
+++ Given/part2/Parser.java
@@ -0,0 +1,60 @@
+/* *** This file is given as part of the programming assignment. *** */
+
+public class Parser {
+
+
+    // tok is global to all these parsing methods;
+    // scan just calls the scanner's scan method and saves the result in tok.
+    private Token tok; // the current token
+    private void scan() {
+        tok = scanner.scan();
+    }
+
+    private Scan scanner;
+    Parser(Scan scanner) {
+        this.scanner = scanner;
+        scan();
+        program();
+        if( tok.kind != TK.EOF )
+            parse_error("junk after logical end of program");
+    }
+
+    private void program() {
+        block();
+    }
+
+    private void block() {
+        // you'll need to add some code here
+    }
+
+    private void declarations() {
+        mustbe(TK.VAR);
+        while( is(TK.ID) ) {
+            scan();
+        }
+        mustbe(TK.RAV);
+    }
+
+    // you'll need to add a bunch of methods here
+
+    // is current token what we want?
+    private boolean is(TK tk) {
+        return tk == tok.kind;
+    }
+
+    // ensure current token is tk and skip over it.
+    private void mustbe(TK tk) {
+        if( ! is(tk) ) {
+            System.err.println( "mustbe: want " + tk + ", got " +
+                                    tok);
+            parse_error( "missing token (mustbe)" );
+        }
+        scan();
+    }
+
+    private void parse_error(String msg) {
+        System.err.println( "can't parse: line "
+                            + tok.lineNumber + " " + msg );
+        System.exit(1);
+    }
+}
