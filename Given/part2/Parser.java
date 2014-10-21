25be9fcb2f455f8ff3684886535cede6303ce83f
Arman Shakirt Kapbasov <akap@pc5.cs.ucdavis.edu>
2014-10-20 23:36:57 -0700

    Part 2 done

--- Given/part2/Parser.java
+++ Given/part2/Parser.java
@@ -6,6 +6,7 @@ public class Parser {
     // tok is global to all these parsing methods;
     // scan just calls the scanner's scan method and saves the result in tok.
     private Token tok; // the current token
+    //private boolean flag = true;
     private void scan() {
         tok = scanner.scan();
     }
@@ -15,11 +16,12 @@ public class Parser {
         this.scanner = scanner;
         scan();
         program();
-	if(tok.kind == TK.NUM)
-	{
-	    parse_error("HI arman");
-	}
-        //if( tok.kind != TK.EOF )
+	//if(tok.kind == TK.NUM)
+	//{
+	  //  parse_error("HI arman");
+	//}
+	//System.out.println("last one: "+ tok.string);
+	if( tok.kind != TK.EOF )
             parse_error("junk after logical end of program");
     }
 
@@ -28,14 +30,218 @@ public class Parser {
     }
 
     private void block() {
-        // you'll need to add some code here
+  	//check to see if declaration
+  	boolean flag = true;
+	if(is(TK.VAR))
+	{
+		declarations();
+		//System.out.println("Works for decleration");
+	}
+	//must be statemnet list
+	while(true){
+		//System.out.println("LOOOOOPIN: "+ tok.string);
+		
+		flag = statement_list(flag);
+		if(is(TK.EOF))
+		{
+			break;
+		}
+		if(flag == false)
+		{
+			break;	
+		}
+	}
     }
 
+    private boolean statement_list(boolean flag)
+    {
+	if(is(TK.ID))
+	{
+		//System.out.println("assignmen: " +tok.string);
+		assignment();
+		return true;
+	}
+	if(is(TK.PRINT))
+	{
+		mustbe(TK.PRINT);
+		expression();
+		return true;
+	}
+	if(is(TK.IF))
+	{
+//		System.out.println("HI");
+		
+		mustbe(TK.IF);
+		guarded_commands();
+		//System.out.println("MUST BE FI: " + tok.string);
+		mustbe(TK.FI);
+		//System.out.println("Whoa there");
+		return true;
+	}
+	if(is(TK.DO))
+	{
+	
+		scan();
+		guarded_commands();
+
+		mustbe(TK.OD);
+		return true;
+	}
+	if(is(TK.FA))
+	{
+		scan();
+		mustbe(TK.ID);
+		mustbe(TK.ASSIGN);
+		expression();
+		mustbe(TK.TO);
+		expression();
+		if(is(TK.ST))
+		{
+			scan();
+			expression();
+		}
+		mustbe(TK.ARROW);
+                block();
+		mustbe(TK.AF);
+		return true;
+	}
+	
+//	System.out.println("ONLY at the end");
+	return false;
+
+    }
+    private void guarded_commands(){
+  		guarded_command();	
+		//System.out.println("NEXT: " + tok.string);
+		while(true)
+		{
+			if(is(TK.BOX)){
+				scan();
+				guarded_command();
+			}	
+			else
+			{
+				break;
+			}
+		}
+		if(is(TK.ELSE))
+		{
+			scan();
+			mustbe(TK.ARROW);
+               		block();
+	
+		}
+	//	System.out.println("NOTHING");	
+	}
+
+    private void guarded_command(){
+		if(is(TK.ELSE))
+		{
+			parse_error("factor");
+		}
+		expression();
+		
+	//	System.out.println("Next: " + tok.string);
+		mustbe(TK.ARROW);
+		block();		
+
+	}
+    //assigning part of statement
+    private void assignment(){
+	//System.out.println("ID" + tok.kind);
+	mustbe(TK.ID);
+	//System.out.println("Next; " + tok.string);
+	mustbe(TK.ASSIGN);
+//	System.out.println("Assigned to : "+ tok.string);
+	expression();
+	
+
+	}
+    private void expression(){
+	
+	//simple
+	simple();
+	//System.out.println("Start of Expression: " + tok.string);
+	//check to see if more than one simple	
+	if(is(TK.EQ) || is(TK.NE) || is(TK.LT) || is(TK.GT) || is(TK.LE) || is(TK.GE)){
+
+	//	System.out.println("CHar :" + tok.string);
+		scan();
+	//	System.out.println("CHar2 :" + tok.string);
+		simple();	
+	}
+	//System.out.println("expression evaluated");
+	 //System.out.println("Next one; "+ tok.string);
+	}//expression()
+
+    private void simple(){
+	boolean flag = true;
+	term();	
+	
+	while(flag)
+	{
+		if(is(TK.PLUS) || is(TK.MINUS))
+		{
+			//System.out.println("PLUS or MINUS: " + tok.string);
+			scan();
+			term();
+		}	
+		else{
+			return;
+		}
+		//scan();	
+	}//while
+	}//simple()
+    private void term(){
+	boolean flag = true;
+	factor();
+
+	while(flag)
+	{
+		//checks for more terms
+		if(is(TK.TIMES) || is(TK.DIVIDE))
+		{ 
+			//System.out.println("Times/divide: " + tok.string);
+ 			scan();
+			factor();	
+		}
+		else 
+		{
+			return;
+		}
+	//	scan();
+	}//while()
+	}//term()
+    private void factor(){
+        if(is(TK.LPAREN))
+	{
+		//System.out.println("left");
+		scan();
+		expression();
+		//scan();
+		//System.out.println("right: " + tok.string);
+
+		mustbe(TK.RPAREN);	
+	 	//System.out.println("right: " + tok.string);	
+	}
+	//id or number
+	else if(is( TK.ID) || is(TK.NUM))
+	{
+		//System.out.println("factor works for id or number: " + tok.string);
+		scan();
+
+	}
+	else
+	{
+		//parse_error("must be ID or NUM");
+	}
+	}//facotr()
     private void declarations() {
         mustbe(TK.VAR);
         while( is(TK.ID) ) {
             scan();
         }
+	//scan();
         mustbe(TK.RAV);
     }
 
